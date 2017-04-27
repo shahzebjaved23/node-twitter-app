2a93c3123019f63851ddb846825ff13d9e2df1a0
shahzeb javed <shahzebjaved23@gmail.com>
2017-04-27 10:48:32 +0500

    added tokena

--- config/env/production.js
+++ config/env/production.js
@@ -5,7 +5,7 @@ module.exports = {
     twitter: {
         consumer_key: process.env.CONSUMER_KEY,
         consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        access_token_key: "197496793-yVnc4tIs1ORhgjJq6ZvpAABADzLVArmD4jIAUULy",
+        access_token_secret: "mn4RpqZfP1NLm6mmZb154uZ4nMNIJ1UHPwPsfopcVwYU7"
     }
 };
