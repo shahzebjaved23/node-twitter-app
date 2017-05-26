9b956a37be87eada4660dc3fb4a67a9d2edffa4c
shahzeb javed <shahzebjaved23@gmail.com>
2017-05-26 17:39:44 +0500

    fixes

--- app/controllers/tweetsController.js
+++ app/controllers/tweetsController.js
@@ -85,16 +85,14 @@ var saveTweetIntoDb = function(tweet,type,callback){
  */
 var TweetsFromStream = [];
 
-function findTweetsBySTREAM(player, team, author) {
-
-    var searchStringSTREAM = player + ' ' + team;
+function findTweetsBySTREAM(query) {
 
     /*
      * Twitter STREAM API, 
      * path: 'statuses/filter', track: to track tweets that match player or team name.
      */
     var stream = TwitterSTREAM.stream('statuses/filter', {
-        track: searchStringSTREAM
+        track: query
     });
 
 
@@ -177,12 +175,12 @@ tweets.getTweetsByRest = function(req,res){
     var team_author_op = req.query.team_author_op;
 
     /*
-    * Open the tweets Stream
+    * Parse the inputs params, replace ',' with AND so that is searches for whole query 
     */
-
-    findTweetsBySTREAM(player, team, author);
-
-
+    player.replace(" "," OR ").replace(","," ");
+    team.replace("FC","").replace("F.C","").replace("F.C.","").replace(" "," OR ").replace(","," ");
+    author.replace(" "," OR ").replace(","," ");
+   
     /*
     * the query words
     */
@@ -193,21 +191,12 @@ tweets.getTweetsByRest = function(req,res){
     *  searching by player, team, or author twitter handles.
     *  appending 'from:' filter to search string to get tweets from given account handles.
     */
-
     var searchStringREST = "";
 
     /*
-    * Parse the inputs params, replace ',' with AND so that is searches for whole query 
-    */
-
-    // TODO: Fix the terms, each term will be considered a single item
-    
-    player.replace(","," ");
-    team.replace("FC","").replace("F.C","").replace("F.C.","").replace(","," ");
-    author.replace(","," ");
-
-    // generate the query string in terms of the supplied operators
-    // leave out the varaibles if they are empty strings
+    *  generate the query string in terms of the supplied operators
+    *  leave out the varaibles if they are empty strings
+    */ 
     if(player_team_op == "AND" && team_author_op == "AND"){
         if(player == "" && team != ""){
             searchStringREST = team+" "+matchWords ;
@@ -252,37 +241,40 @@ tweets.getTweetsByRest = function(req,res){
 
     console.log(searchStringREST);
 
+     /*
+    * Open the tweets Stream
+    */
+
+    findTweetsBySTREAM(searchStringREST);
+
     /*
      * Twitter REST API, 
      * q: searchString, count: Number of tweets returned.
      */
 
-
     var tweetsArray = [];
-    var tweetReturnArray = [];
-
-    TwitterREST.get('search/tweets', { q: searchStringREST , count: 300}, function(error, tweets, response){
+    
+    TwitterREST.get('search/tweets', { q: searchStringREST , count: 300, language:"en"}, function(error, tweets, response){
         
         if(tweets.statuses){
             tweets.statuses.forEach(function(tweet){
                 tweetsArray.push(tweet);
             })
-            TwitterREST.get('search/tweets', { q: searchStringREST , count: 300, max_id: tweetsArray[tweetsArray.length - 1].id}, function(error, tweets, response){
+            TwitterREST.get('search/tweets', { q: searchStringREST , count: 300, max_id: tweetsArray[tweetsArray.length - 1].id, language:"en"}, function(error, tweets, response){
                 if(tweets.statuses){
                     tweets.statuses.forEach(function(tweet){
                         tweetsArray.push(tweet);
                     })
-                    TwitterREST.get('search/tweets', { q: searchStringREST , count: 300, max_id: tweetsArray[tweetsArray.length - 1].id}, function(error, tweets, response){
+                    TwitterREST.get('search/tweets', { q: searchStringREST , count: 300, max_id: tweetsArray[tweetsArray.length - 1].id, language:"en"}, function(error, tweets, response){
                         if(tweets.statuses){
                             tweets.statuses.forEach(function(tweet){
                                 tweetsArray.push(tweet);
                             })
                             console.log(tweetsArray.length);
-                            // console.log(tweetsArray);
-
+                            
+                            // save tweets to db
                             tweetsArray.forEach(function(tweet){
                                 saveTweetIntoDb(tweet,'rest',function(tweet){
-                                    tweetReturnArray.push(tweet);
                                 })
                             })
                             // remove retweets to remove duplicates
@@ -333,15 +325,13 @@ var searchDb = function(player,team,author,player_team_op,team_author_op,callbac
 }
 
 tweets.getTweetsFromDb = function(req,res){
-    var player = req.query.player ? req.query.player : "";
-    var team = req.query.team ? req.query.team : "";
-    var author = req.query.author ? req.query.author : "";
+    var player = req.query.player;
+    var team = req.query.team;
+    var author = req.query.author;
     
     var tweets = searchDb(player,team,author,null,null,function(tweets){
         res.send(tweets);
     });
-    
-
 }
 
 tweets.getFrequency = function(req,res){
